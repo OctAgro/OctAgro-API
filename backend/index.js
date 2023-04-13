01ad29ef56b88d034cc73dd3bf6b9098f667c75c
igorpereira28 <igorsilvape28@gmail.com>
2023-04-13 10:35:27 -0300

    Função de login

--- backend/index.js
+++ backend/index.js
@@ -25,41 +25,8 @@ app.use("/usuario", usuario)
 
 // app.use(express.json())
 
-//session  (onde express vai salvar as sessões)
-// app.use(
-//     session({
-//         name: "session",
-//         secret: "segredo",
-//         resave: false, //caiu a sessão, vai desconectar
-//         saveUninitialized: false,
-//         store: new FileStore({
-//             logFn: function() {},
-//             path:require('path').join(require('os').tmpdir(), 'sessions'),
-//         }),
-//     cookie: {
-//         secure: false,
-//         maxAge: 360000, //equivale a 1 dia
-//         expires: new Date(Date.now() + 360000), //automaticamente vai expirar em 1 dia
-//         httpOnly: true
-//     }
-//     }),
-// )
-
 //mensagens com flash
 app.use(flash())
-
-//path publica
-// app.use(express.static('public'))
-
-//iniciar sessão
-// app.use((req, res, next) => {
-//     //conferir se o usuário possui sessão
-//     if (req.session.usuarioId) {
-//         res.locals.session = req.session
-//     }
-
-//     next()
-// })
 
 //Conexao com Banco de Dados
 conexao.sync().then(() => {

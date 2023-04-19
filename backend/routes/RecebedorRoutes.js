317eb6e767a3bfcc9de771563b148657e41a2fd5
IgorKenzoMS <igor.sasaki@fatec.sp.gov.br>
2023-04-19 12:22:00 -0300

    atualização controller e routes

--- backend/routes/RecebedorRoutes.js
+++ backend/routes/RecebedorRoutes.js
@@ -4,3 +4,5 @@ const RecebedorController = require('../controllers/RecebedorController')
 
 router.get('/home')
 router.post('/entrada-mercadoria', RecebedorController.criarRelatorio)
+router.post('/relatorios/:id', RecebedorController.encontrarRelator)
+router.post('/relatorios/editar/:id', RecebedorController.atualizarRelatorio)

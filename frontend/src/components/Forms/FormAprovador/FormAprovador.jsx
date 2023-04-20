c40267c5ff0a3cf23abececd0fa4e95f7ca955d2
jeejinf <dev.jeejor@gmail.com>
2023-04-19 22:29:03 -0300

    Formatação do FormAprovador

--- frontend/src/components/Forms/FormAprovador/FormAprovador.jsx
+++ frontend/src/components/Forms/FormAprovador/FormAprovador.jsx
@@ -9,12 +9,7 @@ import { Modal } from "../../Modal/Modal"
 
 // IMPORTANDO ICONES
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faEye,
-  faFaceSmileBeam,
-  faFaceFrown,
-  faTriangleExclamation,
-} from "@fortawesome/free-solid-svg-icons"
+import { faEye, faFaceSmileBeam, faFaceFrown, faTriangleExclamation } from "@fortawesome/free-solid-svg-icons"
 
 import styles from "./FormAprovador.module.css"
 
@@ -25,41 +20,30 @@ export const FormAprovador = (props) => {
   const [isAprovadoWarning, setIsAprovadoWarning] = useState(false)
   const [isRecusadoWarning, setIsRecusadoWarning] = useState(false)
 
-  const [
-    checkboxDocumentacaoProdutoAprovado,
-    setCheckboxDocumentacaoProdutoAprovado,
-  ] = useState(false)
+  const [checkboxDocumentacaoProdutoAprovado, setCheckboxDocumentacaoProdutoAprovado] = useState(false)
 
-  const [
-    checkboxDocumentacaoProdutoReprovado,
-    setCheckboxDocumentacaoProdutoReprovado,
-  ] = useState(false)
+  const [checkboxDocumentacaoProdutoReprovado, setCheckboxDocumentacaoProdutoReprovado] = useState(false)
 
-  const [checkboxInfoRecebedorAprovado, setCheckboxInfoRecebedorAprovado] =
-    useState(false)
+  const [checkboxInfoRecebedorAprovado, setCheckboxInfoRecebedorAprovado] = useState(false)
 
-  const [checkboxInfoRecebedorReprovado, setCheckboxInfoRecebedorReprovado] =
-    useState(false)
+  const [checkboxInfoRecebedorReprovado, setCheckboxInfoRecebedorReprovado] = useState(false)
 
-  const [checkboxInfoAnalistaAprovado, setCheckboxInfoAnalistaAprovado] =
-    useState(false)
+  const [checkboxInfoAnalistaAprovado, setCheckboxInfoAnalistaAprovado] = useState(false)
 
-  const [checkboxInfoAnalistaReprovado, setCheckboxInfoAnalistaReprovado] =
-    useState(false)
+  const [checkboxInfoAnalistaReprovado, setCheckboxInfoAnalistaReprovado] = useState(false)
 
   const handleAceitar = (e) => {
     e.preventDefault()
     if (isAprovadoWarning) {
-      // useState que trata se foi aprovado ou recusado pelo aprovador vira false, pois ele não pode aprovar ou recusar ainda, tem que ver o WARNING
+      // useState que trata se foi aprovado ou recusado pelo aprovador vira false,
+      // pois ele não pode aprovar ou recusar ainda, tem que ver o WARNING
       setIsAprovado(false)
       setIsRecusado(false)
 
       // abre modal (mostra o WARNING)
       setOpenModal(true)
-    }
-    else {
+    } else {
       // se nao tiver warning, pode setar isAprovado para true e mostrar o modal
-
       // > implementar logica quando pedido for aceito <
       setIsAprovado(true)
       setIsRecusado(false)
@@ -76,8 +60,7 @@ export const FormAprovador = (props) => {
 
       // abre modal (mostra o WARNING)
       setOpenModal(true)
-    }
-    else {
+    } else {
       // se nao tiver warning, pode setar isRecusado para true e mostrar o modal
 
       // > implementar logica quando pedido for recusado <
@@ -235,88 +218,53 @@ export const FormAprovador = (props) => {
             </fieldset>
 
             <div className={styles.buttons}>
-              <Button
-                value1="RECUSAR"
-                value2="MERCADORIA"
-                onClick={handleRecusar}
-              />
-              <Button
-                value1="ACEITAR"
-                value2="MERCADORIA"
-                onClick={handleAceitar}
-              />
+              <Button value1="RECUSAR" value2="MERCADORIA" onClick={handleRecusar} />
+              <Button value1="ACEITAR" value2="MERCADORIA" onClick={handleAceitar} />
             </div>
           </div>
         </form>
       </div>
       <div className={styles.divModal}>
-        <Modal
-          isOpen={openModal}
-          setModalOpen={() => setOpenModal(!openModal)}
-        >
+        <Modal isOpen={openModal} setModalOpen={() => setOpenModal(!openModal)}>
           <div className={styles.clearfix}>
             {isAprovado && !isAprovadoWarning ? (
               <div className={styles.container}>
-                <FontAwesomeIcon
-                  icon={faFaceSmileBeam}
-                  className={styles.iconSmile}
-                />
-                <p className={styles.paragraph}>
-                  O Pedido {props.numeroPedido} foi aprovado!
-                </p>
+                <FontAwesomeIcon icon={faFaceSmileBeam} className={styles.iconSmile} />
+                <p className={styles.paragraph}>O Pedido {props.numeroPedido} foi aprovado!</p>
                 <Link to="/aprovador/relatorio">
                   <Button className={styles.button} value1="CONFIRMAR" />
                 </Link>
               </div>
             ) : isRecusado && !isRecusadoWarning ? (
               <div className={styles.container}>
-                <FontAwesomeIcon
-                  icon={faFaceFrown}
-                  className={styles.iconSmile}
-                />
-                <p className={styles.paragraph}>
-                  O Pedido {props.numeroPedido} foi recusado!
-                </p>
+                <FontAwesomeIcon icon={faFaceFrown} className={styles.iconSmile} />
+                <p className={styles.paragraph}>O Pedido {props.numeroPedido} foi recusado!</p>
                 <Link to="/aprovador/relatorio">
                   <Button className={styles.button} value1="CONFIRMAR" />
                 </Link>
               </div>
             ) : isAprovadoWarning ? (
               <div className={styles.container}>
-                <FontAwesomeIcon
-                  icon={faTriangleExclamation}
-                  className={styles.iconSmile}
-                />
+                <FontAwesomeIcon icon={faTriangleExclamation} className={styles.iconSmile} />
                 <p className={styles.paragraph}>
                   Tem certeza que deseja aceitar?
                   <br />
                   Essa mercadoria foi recusada pelo Analista.
                 </p>
                 <Link to="/aprovador/relatorio">
-                  <Button
-                    className={styles.button}
-                    value1="CONFIRMAR"
-                    onClick={handleAceitar}
-                  />
+                  <Button className={styles.button} value1="CONFIRMAR" onClick={handleAceitar} />
                 </Link>
               </div>
             ) : isRecusadoWarning ? (
               <div className={styles.container}>
-                <FontAwesomeIcon
-                  icon={faTriangleExclamation}
-                  className={styles.iconSmile}
-                />
+                <FontAwesomeIcon icon={faTriangleExclamation} className={styles.iconSmile} />
                 <p className={styles.paragraph}>
                   Tem certeza que deseja recusar?
                   <br />
                   Essa mercadoria foi aprovada pelo Analista.
                 </p>
                 <Link to="/aprovador/relatorio">
-                  <Button
-                    className={styles.button}
-                    value1="CONFIRMAR"
-                    onClick={handleRecusar}
-                  />
+                  <Button className={styles.button} value1="CONFIRMAR" onClick={handleRecusar} />
                 </Link>
               </div>
             ) : null}

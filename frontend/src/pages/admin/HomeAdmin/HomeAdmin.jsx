6bbdfaa452c22834a14808076899d47d5653d8a6
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2023-05-01 21:34:42 -0300

    módulos do admin

--- frontend/src/pages/admin/HomeAdmin/HomeAdmin.jsx
+++ frontend/src/pages/admin/HomeAdmin/HomeAdmin.jsx
@@ -1,29 +1,21 @@
-import React from "react"
-
-// Importando o Provider
-import { PedidosProvider } from "../../../context/PedidosAnalistaContext"
+import React from "react";
 
 // Importando Componentes
 import { SidebarAdmin } from "../../../components/sidebar/SidebarAdmin/SidebarAdmin"
+import { BodyAdm } from "../../../components/Body/BodyAdm/BodyAdm"
 
 // Importando CSS
 import styles from "./HomeAdmin.module.css"
 
 export const HomeAdmin = () => {
   return (
-    <PedidosProvider>
+    
       <div className={styles.main}>
         <SidebarAdmin />   
         <div className={styles.container}>
-         
-          <h1>Olá Admin</h1>
-        
-          {/* <Header /> */}
-          <div>
-            {/* <body/> */}
-          </div>
+          <BodyAdm />
         </div>
       </div>
-    </PedidosProvider>
+    
   )
 }

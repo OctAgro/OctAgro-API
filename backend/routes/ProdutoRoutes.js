ae07b540eb1809860bdd2a522b0ced0cef6149f3
Igor Pereira <igorpereir28@hotmail.com>
2023-05-03 10:08:32 -0300

    Funcionalidades 'CRUD' do produto

--- backend/routes/ProdutoRoutes.js
+++ backend/routes/ProdutoRoutes.js
@@ -3,5 +3,7 @@ const router = require('express').Router()
 const ProdutoController = require('../controllers/ProdutoController')
 
 router.post('/cadastrarProduto', ProdutoController.cadastrarProduto)
+router.delete('/:id_produto', ProdutoController.deletarProduto)
+router.put('/atualizar/:id_produto', ProdutoController.atualizarProduto)
 
 module.exports = router

051395e7a3c54099ee2d0a53331f81a7d650ff8f
Igor Pereira <igorpereir28@hotmail.com>
2023-05-04 10:07:05 -0300

    Funcionalidade do CRUD do Fornecedor

--- backend/routes/ProdutoRoutes.js
+++ backend/routes/ProdutoRoutes.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 const ProdutoController = require('../controllers/ProdutoController')
 
+router.get('/listar', ProdutoController.listarProduto)
+router.get('/listar/:id_produto', ProdutoController.procurarProduto)
 router.post('/cadastrarProduto', ProdutoController.cadastrarProduto)
 router.delete('/:id_produto', ProdutoController.deletarProduto)
 router.put('/atualizar/:id_produto', ProdutoController.atualizarProduto)

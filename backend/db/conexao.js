1f47785c7e4ba4c0d92c7b480ee5125aaa56235a
Gabriela Barbosa <gabrielasbarbosa4@gmail.com>
2023-05-09 12:10:17 -0300

    atualização do readme preparando para a entrega da 3a sprint

--- backend/db/conexao.js
+++ backend/db/conexao.js
@@ -1,16 +1,19 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize({
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'fatec',
-    database: 'octagro',
-    define: {
-        timestamps: true,
-        freezeTableName: true
-      },
+	dialect: 'mysql',
+	host: 'localhost',
+	port: 3306,
+	username: 'root',
+	password: 'gabriela',
+	database: 'octagro',
+	define: {
+		timestamps: true,
+		freezeTableName: true
+	},
 })
 
 module.exports = sequelize

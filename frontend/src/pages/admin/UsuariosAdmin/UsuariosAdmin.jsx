baaa3210b12bb2830df7e5d4980915ec494d9912
jeejinf <dev.jeejor@gmail.com>
2023-05-09 08:58:59 -0300

    Criação da barra de pesquisa do admin para a parte de 'Usuários'

--- frontend/src/pages/admin/UsuariosAdmin/UsuariosAdmin.jsx
+++ frontend/src/pages/admin/UsuariosAdmin/UsuariosAdmin.jsx
@@ -6,6 +6,11 @@ import { UsuariosCadastradosProvider } from "../../../context/UsuariosCadastrado
 // Importando Componentes
 import { SidebarAdmin } from "../../../components/sidebar/SidebarAdmin/SidebarAdmin"
 import { HeaderUsuarios } from "../../../components/header/HeaderAdmin/HeaderUsuarios/HeaderUsuarios"
+import { BarraAdmin } from "../../../components/BarraAdmin/BarraAdmin"
+
+// Importando ícones
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faUserPlus } from "@fortawesome/free-solid-svg-icons"
 
 // Importando CSS
 import styles from "./UsuariosAdmin.module.css"
@@ -14,12 +19,19 @@ export const UsuariosAdmin = () => {
   return (
     <UsuariosCadastradosProvider>
       <div id={styles["main"]}>
-        <div id={styles["sidebar"]}><SidebarAdmin /></div>
-        <div id={styles["header"]}><HeaderUsuarios /></div>
-        <div id={styles["barraPesquisa"]}>Barra Pesquisa</div>
-        <div id={styles["body"]}>
-        {/*  < Body /> */}  
-        </div>          
+        <div id={styles["sidebar"]}>
+          <SidebarAdmin />
+        </div>
+        <div id={styles["header"]}>
+          <HeaderUsuarios />
+        </div>
+        <div id={styles["barraPesquisa"]}>
+          <BarraAdmin linkVoltar="/admin/usuarios" linkCadastrar="/admin/usuarios/cadastrousuarios">
+            <FontAwesomeIcon icon={faUserPlus} title="Cadastrar novo usuário!" />
+          </BarraAdmin>
+        </div>
+
+        <div id={styles["body"]}>{/*  < Body /> */}</div>
       </div>
     </UsuariosCadastradosProvider>
   )

87a958dbab0db4bdacf079733882b7a5e4b70728
Briscese <gabrielbrosig@gmail.com>
2023-05-13 09:03:27 -0300

    Atualização de tabela de Usuario para ficar igual ao Figma e tbm alteração das rotas de ADM para o CRUD de usuarios

--- backend/routes/AdministradorRoutes.js
+++ backend/routes/AdministradorRoutes.js
@@ -4,6 +4,7 @@ const router = require ('express').Router()
 const FornecedorController = require('../controllers/FornecedorController')
 const PedidoController = require('../controllers/PedidoController')
 const ProdutoController = require('../controllers/ProdutoController')
+const UsuarioControllers = require('../controllers/UsuarioController')
 
 // Rotas do Administrador para fazer CRUD de Fornecedores
 
@@ -22,12 +23,20 @@ router.post('/pedidos/atualizar/:id',PedidoController.atualizarPedido) // Atuali
 router.post('/pedidos/deletar/:id', PedidoController.alterarStatusPedido) // Deletar um Pedido
 
 
-// Rotas do Arministrador para fazer CRUD do Produto
+// Rotas do Administrador para fazer CRUD do Produto
 
 router.get('/produtos/home',ProdutoController.listarProduto) // Listar Produtos
 router.post('/produtos/cadastrar',ProdutoController.cadastrarProduto) // Cadastrar Produto
 router.post('/produtos/atualizar/:id',ProdutoController.atualizarProduto) // Atualizar Produto
 router.post('/produtos/deletar/:id',ProdutoController.alterarStatusProduto) // Deletar Produto
 router.post('/produtos/procurar/:id',ProdutoController.procurarProduto) // Achar um Produto Produto
+
+// Rotas do Administrador para fazer CRUD de Usuario
+
+router.post('/usuarios/cadastrar',UsuarioControllers.registrarUsuarioPost) // Cadastrar Usuario
+router.post('/usuarios/buscar', UsuarioControllers.buscarUsuarioByEmail) // Buscar Usuario por email
+router.get('/usuarios/listar',UsuarioControllers.listarUsuario) // Listar todos os usuarios
+router.post('/usuarios/deletar/:id',UsuarioControllers.deletarUsuario) // deletar usuario
+router.post('/usuarios/atualizar/:id',UsuarioControllers.atualizarUsuario) // Atualizar Usuario
 
 module.exports = router
